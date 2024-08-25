6cd45ca0e36053754594e678ff4a991b5ed74b0d
choudhary-hema <65819733+choudhary-hema@users.noreply.github.com>
2024-08-25 15:49:05 +0530

    Update FinalClassExample.java

--- FinalClassExample.java
+++ FinalClassExample.java
@@ -1,15 +1,24 @@
 final class Cat {
 
+    // Private field to store the weight of the cat
     private int weight;
+
+    // Default constructor for the Cat class
     public Cat(){
         
     }
+
+    // Constructor that takes an integer parameter for weight
     public Cat(int weight){
         this.weight = weight;
     }
+
+    // Method to get the weight of the cat
     public int getWeight(){
         return this.weight;
     } 
+
+    // Method to set the weight of the cat
     public void setWeight(int weight){
         this.weight = weight;
     }
@@ -17,9 +26,13 @@ final class Cat {
 
 public class FinalClassExample {
     public static void main (String a[]){
+        // Create a new Cat object with an initial weight of 5
         Cat cat1 = new Cat(5);
-        System.out.println("Weight of the cat : " + cat1.getWeight());
+        // Print the weight of the cat
+        System.out.println("Weight of the cat :  " + cat1.getWeight());
+        // Set the weight of the cat to 1
         cat1.setWeight(1);
-        System.out.println("Weight of the cat : " + cat1.getWeight());
+        // Print the updated weight of the cat
+        System.out.println("Weight of the cat :  " + cat1.getWeight());
     }
 }
